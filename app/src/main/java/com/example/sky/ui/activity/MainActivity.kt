112366a37b2e651e8988e4a8e2078782783fa5b7
adamkis <kisferencadam@gmail.com>
2018-02-03 17:54:44 +0100

    Skeleton of toolbar

--- app/src/main/java/com/example/sky/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/sky/ui/activity/MainActivity.kt
@@ -3,8 +3,12 @@ package com.example.sky.ui.activity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import com.example.sky.R
 import com.example.sky.ui.fragment.SearchFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import android.support.v4.view.MenuItemCompat
+import android.view.Menu
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +19,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
         replaceFragment(
             if(savedInstanceState != null) supportFragmentManager.getFragment(savedInstanceState, ACTIVE_FRAGMENT_KEY)
             else SearchFragment.newInstance()
@@ -29,6 +37,11 @@ class MainActivity : AppCompatActivity() {
     private fun replaceFragment(fragment: Fragment){
         activeFragment = fragment
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, activeFragment).commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
 }
